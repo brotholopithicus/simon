6f36663683a97f3d60188916447613ffe14a64b6
James Jarvis <jamespjarvis@gmail.com>
2016-10-07 19:49:15 -0400

    fixed sockets

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* ========= MODULES ========= */
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -6,16 +7,21 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Score = require('./models/Scores');
 
+// connect to mongodb
 mongoose.connect('mongodb://localhost:27017/scores');
 
+// configure static files and views
 app.use(express.static(path.join(__dirname + '/public')));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname + '/views'));
 
+/* ========= ROUTES ========= */
+// root
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// api endpoint for high scores
 app.get('/scores', (req, res) => {
     Score.find({}, (err, scores) => {
         if (err) throw err;
@@ -24,6 +30,8 @@ app.get('/scores', (req, res) => {
     });
 });
 
+/* ========= SOCKETS ========= */
+// configure client / server socket events
 io.on('connection', (socket) => {
     console.log('user connected');
     socket.on('score', (newScore) => {
@@ -43,6 +51,7 @@ io.on('connection', (socket) => {
     });
 });
 
+// LISTEN
 server.listen(3000, () => {
     console.log('sever listening on port 3000');
 });
